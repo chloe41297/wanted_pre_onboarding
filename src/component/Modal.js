7340bb13a08ba918f76101d758a02c957ec20790
chloe41297 <79011228+chloe41297@users.noreply.github.com>
2022-01-27 21:37:57 +0900

    feat:modal component 제작

---
+++ src/component/Modal.js
@@ -0,0 +1,72 @@
+import styled from "@emotion/styled";
+import { useState } from "react";
+
+const Modal = () => {
+  const [opened, setOpened] = useState(false);
+  const handleOpen = () => {
+    setOpened(true);
+  };
+  const handleClose = (e) => {
+    if (e.target.className.match("ModalClose")) setOpened(false);
+  };
+  return (
+    <>
+      <h2>Modal</h2>
+      {!opened ? (
+        <ModalButton onClick={handleOpen}>Open Modal</ModalButton>
+      ) : (
+        <ModalBackground className="ModalClose" onClick={handleClose}>
+          <ModalContainer>
+            <div>
+              Hello!
+              <br /> "x"버튼이나 모달 바깥 영역을 클릭하여 창을 닫아 주세요!
+            </div>
+            <ModalClose className="ModalClose" onClick={handleClose}>
+              X
+            </ModalClose>
+          </ModalContainer>
+        </ModalBackground>
+      )}
+    </>
+  );
+};
+const ModalButton = styled.button`
+  border: none;
+  width: 100px;
+  height: 40px;
+  border-radius: 20px;
+  color: whitesmoke;
+  font-weight: 600;
+  background-color: #e66767;
+`;
+const ModalBackground = styled.div`
+  background-color: rgba(0, 0, 0, 0.4);
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const ModalContainer = styled.div`
+  width: 400px;
+  height: 300px;
+  background-color: whitesmoke;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 10px;
+`;
+const ModalClose = styled.button`
+  border: solid 1px rgba(0, 0, 0, 0.2);
+  padding: 5px 8px;
+  position: absolute;
+  right: 8px;
+  top: 8px;
+  border-radius: 5px;
+`;
+
+export default Modal;
--- src/component/Toggle.js
+++ src/component/Toggle.js
@@ -1,13 +1,23 @@
 import styled from "@emotion/styled";
+import { useState } from "react";
 
 const Toggle = () => {
+  const [checked, setChecked] = useState(false);
+  const handleChange = () => {
+    setChecked(!checked);
+  };
   return (
     <>
       <h2>Toggle</h2>
       <ToggleContainer>
-        <ToggleInput type="checkbox"></ToggleInput>
+        <ToggleInput
+          type="checkbox"
+          checked={checked}
+          onChange={handleChange}
+        ></ToggleInput>
         <ToggleBackground></ToggleBackground>
       </ToggleContainer>
+      <div>{checked ? "Toggle Switch ON" : "Toggle Switch OFF"}</div>
     </>
   );
 };
@@ -15,7 +25,7 @@ const ToggleContainer = styled.label`
   display: inline-block;
 `;
 const ToggleBackground = styled.div`
-  width: 100px;
+  width: 80px;
   height: 35px;
   padding: 5px 0px 0px 5px;
   border-radius: 20px;
@@ -35,7 +45,7 @@ const ToggleBackground = styled.div`
 const ToggleInput = styled.input`
   display: none;
   &:checked + div {
-    background-color: #f7b731;
+    background-color: #596275;
   }
   &:checked + div:after {
     left: calc(100% - 35px);
